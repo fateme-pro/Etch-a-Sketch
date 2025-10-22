fc2d4e41b489450010c4ec75c8bff79c47436fce
fateme-pro <fateme.samifard@gmail.com>
2025-10-22 10:21:17 +0330

    Create popUp function to take user's grid creat user's grid with creatGrid function calculate each square size with const squareSize

--- java.js
+++ java.js
@@ -1,20 +1,57 @@
-const container=document.querySelector(".container");
-container.style.display="flex";
-container.style.flexWrap="wrap";
-container.style.justifyContent="flex-start";
-container.style.width="680px";
-container.style.height="680px";
-container.style.margin = "20px auto";
+function popUp(){
+ const body= document.querySelector("body");
+ const button=document.querySelector("button")
+ body.appendChild(button);
+ button.textContent="click here to design your own grid!!";
+ button.addEventListener("click",()=>{
+   button.textContent="if you want another grid click here";
+    let numPerSide=prompt("number of squares per side?","16*16");
+    numPerSide=parseInt(numPerSide);
+    if(!isNaN(numPerSide)&&numPerSide>0){
+      createGrid(numPerSide);
+    }else{
+      alert("please enter a valid number");
+    }
+ 
+ });
+
+}
 
 
-for(let i=0;i<=255;i++){
+
+
+function createGrid(num){
+ const container=document.querySelector(".container");
+ container.innerHTML="";//clear page
+ container.style.display="flex";
+ container.style.flexWrap="wrap";
+ container.style.justifyContent="flex-start";
+ container.style.width="800px";
+ container.style.height="800.5px";
+ container.style.margin = "20px auto";
+
+ const  squareSize= 800/num;
+
+for(let i=1;i<=(num*num);i++){
  const div=document.createElement("div");
  div.classList.add("square");
  container.appendChild(div);
- div.style.backgroundColor="green";
- div.style.width="40px";
- div.style.height="40px";
+ div.style.backgroundColor="#dbb7db";
+ div.style.width=`${squareSize}px`;
+ div.style.height=`${squareSize}px`;
  div.style.margin = "0";
- div.style.border="1px solid red";
+ div.style.border="1px solid #868286";
+ div.style.boxSizing="border-box"
+ div.addEventListener("mouseenter",()=>{
+   div.classList.add("hover"); 
+ })
+ div.addEventListener("mouseover",()=>{
+    div.classList.remove("hover");
+ })
+}
 
 }
+
+popUp();
+
+
